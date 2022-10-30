4a1faddb79ba61fd5ce19eeed0fe7bd6cf16aa60
bhaveshchit <chittorabhavesh24@gmail.com>
2022-10-30 20:23:43 +0530

    final commit

--- sanity-blog-website/schemas/blog.js
+++ sanity-blog-website/schemas/blog.js
@@ -1,93 +1,99 @@
 export default {
-    name:"blog",
-    type:'document',
-    title:'Blog',
-    fields:[
-        {
-            name:'title',
-            type:'string',
-            title:'Title'
-        },
-        {
-            name: 'content',
-            title: 'Content',
-            type: 'array',
-            of: [
-              {
-                type: 'block'
-              },
-              {
-                type: 'image',
-                fields: [
-                  {
-                    type: 'text',
-                    name: 'alt',
-                    title: 'Alternative text',
-                    description: `Some of your visitors cannot see images, 
-                      be they blind, color-blind, low-sighted; 
-                      alternative text is of great help for those 
-                      people that can rely on it to have a good idea of 
-                      what\'s on your page.`,
-                    options: {
-                      isHighlighted: true
-                    }
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            title:'metadescription',
-            name:'Metadec',
-            type:'string'
-          },
-        {
-            name:'createdAt',
-            type:'datetime',
-            title:'createdAt',
-            options: {
-                dateFormat: 'YYYY-MM-DD',
-                timeFormat: 'HH:mm',
-                timeStep: 15,
-                calendarTodayLabel: 'Today'
-              }
-        },
-
-        {
-            title: 'BlogImage',
-            name: 'blogimage',
-            type: 'image',
-            options: {
-              hotspot: true // <-- Defaults to false
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+      {
+          name: 'title',
+          type: 'string',
+          title: 'Title'
+      },
+      {
+        title: 'Slug',
+        name: 'slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 200, // will be ignored if slugify is set
+          slugify: input => input
+                               .toLowerCase()
+                               .replace(/\s+/g, '-')
+                               .slice(0, 200)
+        }
+      },
+      {
+          name: 'content',
+          title: 'Content',
+          type: 'array',
+          of: [
+            {
+              type: 'block'
             },
-            fields: [
-              {
-                name: 'caption',
-                type: 'string',
-                title: 'Caption',
-                options: {
-                  isHighlighted: true // <-- make this field easily accessible
+            {
+              type: 'image',
+              fields: [
+                {
+                  type: 'text',
+                  name: 'alt',
+                  title: 'Alternative text',
+                  description: `Some of your visitors cannot see images, 
+                    be they blind, color-blind, low-sighted; 
+                    alternative text is of great help for those 
+                    people that can rely on it to have a good idea of 
+                    what\'s on your page.`,
+                  options: {
+                    isHighlighted: true
+                  }
                 }
-              },
-              {
-                // Editing this field will be hidden behind an "Edit"-button
-                name: 'attribution',
-                type: 'string',
-                title: 'Attribution',
-              }
-            ]
+              ]
+            }
+          ]
+        },
+        {
+          name: 'metadesc',
+          type: 'string',
+          title: 'Meta Description'
+      },
+        {
+          title: 'Blog Image',
+          name: 'blogimage',
+          type: 'image',
+          options: {
+            hotspot: true // <-- Defaults to false
           },
-          {
-            name: 'author',
-            type: 'object',
-            fields: [
-              {
-                title: 'Author',
-                name: 'author',
-                type: 'reference',
-                to: [{type: 'author'}]
+          fields: [
+            {
+              name: 'caption',
+              type: 'string',
+              title: 'Caption',
+              options: {
+                isHighlighted: true // <-- make this field easily accessible
               }
-            ]
-          },
-    ]
+            },
+            {
+              // Editing this field will be hidden behind an "Edit"-button
+              name: 'attribution',
+              type: 'string',
+              title: 'Attribution',
+            }
+          ]
+        },
+      {
+          title: 'Created At',
+          name: 'createdAt',
+          type: 'datetime', 
+      },
+      {
+          name: 'author',
+          type: 'object',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'author'}]
+            }
+          ]
+        }
+  ]
 }
